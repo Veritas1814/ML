a9d9ebc8762ab81d0f0812e70b43f19d8fdd0310
Matvii Shumylovych <149301025+Veritas1814@users.noreply.github.com>
2025-10-19 19:16:14 +0300

    Delete reconstruction directory

--- reconstruction/block_palette.py
+++
@@ -1,6 +0,0 @@
-BLOCK_PALETTE = {
-0: 'minecraft:air',
-1: 'minecraft:stone_bricks',
-2: 'minecraft:glass_pane',
-3: 'minecraft:oak_door',
-}
--- reconstruction/mask_to_3d.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def mask_to_voxels(mask, height=10):
-    h, w = mask.shape
-    voxels = np.zeros((height, h, w), dtype=int)
-    for z in range(height):
-        voxels[z][mask == 1] = 1
-
-    return voxels
-
