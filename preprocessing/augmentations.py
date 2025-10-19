1d3d4c53932e6fe75aab1fee7944791d20a67ca6
Matvii Shumylovych <149301025+Veritas1814@users.noreply.github.com>
2025-10-19 19:16:24 +0300

    Delete preprocessing directory

--- preprocessing/augmentations.py
+++
@@ -1,7 +0,0 @@
-import albumentations as A
-def get_training_augmentations(image_size=512):
-    return A.Compose([
-        A.RandomRotate90(p=0.5),
-        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=10, p=0.7),
-        A.RandomBrightnessContrast(p=0.5),
-    ])
