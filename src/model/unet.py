7d823a007bc77a4316dfb4c31a81fee0cfe08c82
Matvii Shumylovych <149301025+Veritas1814@users.noreply.github.com>
2025-10-20 15:22:25 +0300

    Refactor UNet class for improved readability

--- src/model/unet.py
+++ src/model/unet.py
@@ -5,25 +5,20 @@ class UNet(nn.Module):
     def __init__(self, in_channels=3, num_classes=4):
         super(UNet, self).__init__()
         
-        # Encoder
         self.enc1 = self._block(in_channels, 64)
         self.enc2 = self._block(64, 128)
         self.enc3 = self._block(128, 256)
         self.enc4 = self._block(256, 512)
         
-        # Bottleneck
         self.bottleneck = self._block(512, 1024)
         
-        # Decoder - adjust input channels for concatenation
-        self.dec4 = self._block(1024 + 512, 512)  # 1024 from bottleneck + 512 from skip connection
-        self.dec3 = self._block(512 + 256, 256)   # 512 from dec4 + 256 from skip connection
-        self.dec2 = self._block(256 + 128, 128)   # 256 from dec3 + 128 from skip connection
-        self.dec1 = self._block(128 + 64, 64)     # 128 from dec2 + 64 from skip connection
+        self.dec4 = self._block(1024 + 512, 512)  
+        self.dec3 = self._block(512 + 256, 256)   
+        self.dec2 = self._block(256 + 128, 128)   
+        self.dec1 = self._block(128 + 64, 64)     
         
-        # Final layer
         self.final = nn.Conv2d(64, num_classes, kernel_size=1)
         
-        # Max pooling and upsampling
         self.pool = nn.MaxPool2d(2)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         
@@ -38,19 +33,17 @@ class UNet(nn.Module):
         )
     
     def forward(self, x):
-        # Encoder
         e1 = self.enc1(x)
         e2 = self.enc2(self.pool(e1))
         e3 = self.enc3(self.pool(e2))
         e4 = self.enc4(self.pool(e3))
         
-        # Bottleneck
         b = self.bottleneck(self.pool(e4))
         
-        # Decoder with skip connections
         d4 = self.dec4(torch.cat([self.upsample(b), e4], dim=1))
         d3 = self.dec3(torch.cat([self.upsample(d4), e3], dim=1))
         d2 = self.dec2(torch.cat([self.upsample(d3), e2], dim=1))
         d1 = self.dec1(torch.cat([self.upsample(d2), e1], dim=1))
         
+
         return self.final(d1)
